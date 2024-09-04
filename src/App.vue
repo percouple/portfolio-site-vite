2e31362ffe36e3d7903b7938894c6d6fe2386d97
Caleb Evans <calebsevan@gmail.com>
2024-09-03 20:16:14 -0500

    Home.js to Landing.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 </script>
 
 <template>
   <!-- NavBar -->
   <!-- BodyWithSide -->
     <!-- Social Links -->
+    <Landing />
     <!-- Home -->
     <!-- About -->
     <!-- Projects -->
@@ -14,16 +15,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex justify-center mt-[13rem] mb-[18rem] 
+  bg-[#222222] pb-[13px] scroll-mt-[25vh]">
+    <div class="flex flex-col" id="home">
+      <div class="ml-[20px]">
+        <h3 class="mb-0 italic text-lg">
+          Hello, I'm
+        </h3>
+        <div class="text-[#f17f29] text-[8rem] m-0 
+        custom-shadow">
+          Caleb.</div>
+      </div>
+      <div class="flex justify-end text-[1rem] text-right 
+        p-[8px] ml-[30%] bg-[#222222] 
+        shadow-[2px_2px_0px_#5c95ff] max-w-[50%]">
+        I'm a software developer eager to craft
+        engaging experiences tailored
+        for entry-level learners. <br></br>
+        <br></br>Currently, I'm building projects geared
+        towards expanding my proficiency across various
+        coding languages and platforms, enriching
+        my versatility in software development.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.custom-shadow {
+  text-shadow: 3px 2px 0 #bdc4a7;
+}
+</style>
