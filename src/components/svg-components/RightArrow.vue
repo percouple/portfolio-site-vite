80b062859ee2523aae0b292d4d4f2a2afe1cdc28
Caleb Evans <calebsevan@gmail.com>
2024-11-09 14:36:43 -0600

    readme and right arrow svg creation

---
+++ src/components/svg-components/RightArrow.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps(['url'])
+const onClick = () => {
+    window.location.href = props.url;
+} 
+</script>
+
+<template>
+    <button class="custom-svg-rules {} mt-2 p-1 border-2" :style="{
+        display: props.url ? 'inline-block' : 'none'
+    }" @click="onClick">
+        <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        height="24px" 
+        viewBox="0 -960 960 960" 
+        width="24px" 
+        fill="#e8eaed">
+            <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+        </svg>
+    </button>
+</template>
+
+<style scoped></style>
