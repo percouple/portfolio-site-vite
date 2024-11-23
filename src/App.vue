ca982f4a14eb764dbbab49c6249414f00796c994
Caleb Evans <calebsevan@gmail.com>
2024-11-23 10:47:45 -0600

    Password page creation + scaffolding

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,18 @@
 import About from './components/main-components/About.vue';
 import Connect from './components/main-components/footer/Connect.vue';
 import Landing from './components/main-components/Landing.vue';
+import PasswordPage from './components/main-components/PasswordPage.vue';
 import ProjectsContainer from './components/main-components/ProjectsContainer.vue';
+import { ref } from 'vue';
+
+const authenticated = ref(false);
 </script>
 
 <template>
-  <div class="flex flex-col justify-center items-center">
+  <div v-if="!authenticated">
+    <PasswordPage :authenticated="authenticated" />
+  </div>
+  <div v-if="authenticated" class="flex flex-col justify-center items-center">
     <!-- NavBar -->
     <!-- BodyWithSide -->
     <!-- Social Links -->
---
+++ src/components/main-components/PasswordPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const userInput = ref('');
+</script>
+
+<template>
+<div>
+    This is my password page
+</div>
+</template>
+
+<style scoped></style>
